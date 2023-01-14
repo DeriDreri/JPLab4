0bfef0f740ef7846f041dddbf7f965c4c82807f9
Dominik Cybulski <dominikcybulski@protonmail.com>
2023-01-14 22:08:04 +0100

    Adds parameters to ChartPanel

--- JPLab4/src/ad2/ChartPanel.java
+++ JPLab4/src/ad2/ChartPanel.java
@@ -7,10 +7,14 @@ import javax.swing.border.LineBorder;
 
 public class ChartPanel extends JPanel{
 
+	private static final long serialVersionUID = 1L;
+	final int PANEL_WIDTH = 652;
+	final int PANEL_HEIGHT = 265;
+	
 	
 	public ChartPanel() {
 		this.setBackground(new Color(255, 255, 255));
 		this.setBorder(new LineBorder(new Color(0, 0, 0)));
-		this.setBounds(10, 11, 652, 265);
+		this.setBounds(10, 11, PANEL_WIDTH, PANEL_HEIGHT);
 	}
 }
